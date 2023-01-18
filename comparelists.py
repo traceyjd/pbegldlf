49529b2e16321a12d32bf2b0cbf264cd341a330f
Tracey <tracey.tdoherty@gmail.com>
2023-01-18 09:49:14 +0000

    Lists Dictionaries Loops Functions Files Modules

---
+++ comparelists.py
@@ -0,0 +1,15 @@
+
+# Comparing Lists
+
+# How do we check if two lists are equal????
+# They have to have the same items and in exactly the same order
+
+my_list = [1, 2, 3, 4]
+your_list = [4, 3, 2, 1]
+his_lst = [1, 2, 3, 4]
+
+print(my_list == your_list) # evaluates to false
+print(my_list == his_lst) # evaluates to true
+
+
+
