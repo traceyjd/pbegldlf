73f06a3c30eae638bad0d76285a8a1f6867b0baf
Tracey <tracey.tdoherty@gmail.com>
2023-01-18 14:46:37 +0000

    Lists Dictionaries Loops Functions Files Modules

--- LooptyLoops.py
+++ LooptyLoops.py
@@ -1,7 +1,13 @@
 
 # For loop - For each item in our list do this
-
+# Average price
+total = 0
 prices = [2.50, 3.50, 4.50]
 
 for price in prices:
     print('Price is', price)
+    total = total + price
+    print('total is', total)
+
+average = total/len(prices) # The length function gives us all the prices in the list
+print('average is', average)
