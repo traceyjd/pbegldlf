6c2a86a90f66f6d3aa5872569d69e4421eec8465
Tracey <tracey.tdoherty@gmail.com>
2023-01-18 15:03:49 +0000

    Lists Dictionaries Loops Functions Files Modules

---
+++ randomraffle.py
@@ -0,0 +1,13 @@
+
+
+# Generate a randome raffle ticket
+import random
+
+for i in range(10):
+    ticket = random.randint(1,1000)
+    print(ticket)
+
+# To generate a list of years in steps of 2 years with a start value and end value
+for i in range(2005, 2016, 2): # from 2005 to 2016 in steps of 2 years print it all out
+    print(i)
+
