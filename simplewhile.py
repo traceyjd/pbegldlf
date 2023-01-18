377b0f22b53db2111cd2deb389cfd49c74d0473c
Tracey <tracey.tdoherty@gmail.com>
2023-01-18 16:32:20 +0000

    Lists Dictionaries Loops Functions Files Modules

---
+++ simplewhile.py
@@ -0,0 +1,13 @@
+
+# While loopy loops
+
+# While loops continue looping while a condition is True
+
+x = 1
+while x != 3: # like saying do this as long as x does not equal 3
+    print('x is', x)
+    x = x + 1  # The loop would run forever unless we changed x so the contitional could eventually be false.
+
+# 1st loop x is 1
+# 2nd loop x is 2
+# 3rd loop loop is done running as it now equals 3, our conditional was set to not equal 3 keep looping
