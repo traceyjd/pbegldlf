67849391fcf2af4f9e12ae68db2a370cd4396405
Tracey <tracey.tdoherty@gmail.com>
2023-01-25 15:23:35 +0000

    Lists Dictionaries Loops Functions Files Modules

--- ordersbreak.py
+++ ordersbreak.py
@@ -1,11 +1,10 @@
 
 menu = {'Knackered Spam': 0.5, 'Pip Spam': 1.5, 'Squidgy Spam': 2.5, 'Smashing Spam': 3.5, 'Cheeky Spam': 4.5}
 orders = []
-
+order = input('What would you like to order? (Q to Quit)')
 
 while (True):
-    order = input('What would you like to order? (Q to Quit)')
-
+    order = input('Anything else? (Q to Quit)')
     if order == 'Cheeky Spam':
         print('Sorry, we are all out of that!')
         continue
@@ -25,6 +24,6 @@ while (True):
     # If we dont change "order" in the loop
 
     # See if the customer want to order antything else
-    order = input('Anything else? (Q to Quit)')
+
 
 print(orders)
