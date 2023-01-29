80ad225528c030c690c582f2f460e093a975b38c
Tracey <tracey.tdoherty@gmail.com>
2023-01-29 15:50:54 +0000

    Lists Dictionaries Loops Functions Files Modules

---
+++ recent.py
@@ -0,0 +1,16 @@
+
+def average(numbers):
+    total = 0
+    for num in numbers:
+        total = total + num
+
+    avg = total/len(numbers)
+    return avg
+
+# Use our function on prices
+prices = [2.50, 3, 4.50, 5] #  First line of code thats run
+
+result = average(prices)
+
+
+print(result)
