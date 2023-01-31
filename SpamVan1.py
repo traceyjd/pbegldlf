4dc6da931dece5bdb3b062862fa3b5dc89066e14
Tracey <tracey.tdoherty@gmail.com>
2023-01-31 14:06:48 +0000

    Lists Dictionaries Loops Functions Files Modules

--- SpamVan1.py
+++ SpamVan1.py
@@ -4,14 +4,12 @@
 
 #  Calculate the total bill
 
-menu = {'Knackered Spam': 0.5, 'Pip Spam': 1.5, 'Squidgy Spam': 2.5, 'Smashing Spam': 3.5, 'Cheeky Spam': 4.5}
-
-def print_menu(menu)
-    for name, price in menu.items();
-        print(name, ':$', format(price, '.2f'), sep='')  # indented : contents of the function are indented
+def print_menu(menu):
+    for name, price in menu.items():
+        print(name, ': $', format(price, '.2f'), sep='')  # indented : contents of the function are indented
     # We dont need to return anything as we are just printing
 
-def get_order(menu)
+def get_order(menu):
     orders = []
     order = input('What would you like to order? (Q to Quit)')
 
@@ -27,3 +25,12 @@ def get_order(menu)
         # If we dont change "order" in the loop
         # See if the customer want to order antything else
         order = input('Anything else? (Q to Quit)')
+    return orders
+
+def main():
+    menu = {'Knackered Spam': 0.5, 'Pip Spam': 1.5, 'Squidgy Spam': 2.5, 'Smashing Spam': 3.5, 'Cheeky Spam': 4.5}
+    print_menu(menu)
+    order = get_order(menu)
+    print('You ordered:',order)
+
+main()
