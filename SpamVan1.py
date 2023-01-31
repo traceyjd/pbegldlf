5f19be141183df96fbd4e1d695040219e0308b90
Tracey <tracey.tdoherty@gmail.com>
2023-01-31 13:48:12 +0000

    Lists Dictionaries Loops Functions Files Modules

---
+++ SpamVan1.py
@@ -0,0 +1,29 @@
+#  Print the menu
+
+#  Take an order
+
+#  Calculate the total bill
+
+menu = {'Knackered Spam': 0.5, 'Pip Spam': 1.5, 'Squidgy Spam': 2.5, 'Smashing Spam': 3.5, 'Cheeky Spam': 4.5}
+
+def print_menu(menu)
+    for name, price in menu.items();
+        print(name, ':$', format(price, '.2f'), sep='')  # indented : contents of the function are indented
+    # We dont need to return anything as we are just printing
+
+def get_order(menu)
+    orders = []
+    order = input('What would you like to order? (Q to Quit)')
+
+    while (order.upper() != 'Q'):
+        # Find the order and add it to the list if it exists
+        found = menu.get(order)  # Try to get the menu item
+        if found:
+            orders.append(order)  # If it exists, add it to the list
+        else:
+            print("Menu item doesn't exist")
+
+        # See if the customer wants to order anything else
+        # If we dont change "order" in the loop
+        # See if the customer want to order antything else
+        order = input('Anything else? (Q to Quit)')
