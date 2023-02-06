542f7c21e54e1d3e38caaf8cb700536ee3e77467
Tracey <tracey.tdoherty@gmail.com>
2023-02-06 10:40:45 +0000

    Lists Dictionaries Loops Functions Files Modules

--- read_for_pound.py
+++ read_for_pound.py
@@ -2,7 +2,8 @@
 def read_pound_menu()
     pound_spam = open('pound_menu.txt', 'r')
 
-    for line in dollar_spam:
+    pound_menu = []
+    for line in pound_menu:
         print(line)
 
     pound_spam.close()
