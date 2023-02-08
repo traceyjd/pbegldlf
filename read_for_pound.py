2995fe32dc806423beaa95d044f77fb6a25d9379
Tracey <tracey.tdoherty@gmail.com>
2023-02-08 14:55:39 +0000

    Lists Dictionaries Loops Functions Files Modules

--- read_for_pound.py
+++ read_for_pound.py
@@ -2,7 +2,11 @@
 def read_pound_menu():
     pound_spam = open('pound_menu.txt', 'r')
 
+    pound_menu = []
     for line in pound_spam:
-        print(line)
+        line = line.strip()     # This will strip white space including new line characters
+        pound_menu.append(line)
 
+
+    print(pound_menu)
     pound_spam.close()
